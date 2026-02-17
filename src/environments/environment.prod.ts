b157a0afd409bd8747cddb264e02870d450799d7
El Tucu <guidomuratore@gmail.com>
2026-02-17 19:00:29 -0300

    Produccion MercadoPago

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    mpPublicKey: 'APP_USR-7298da68-4cca-48bf-a7ab-7548ab95dbbf',
+    mpPublicKey: 'APP_USR-72e88804-b58e-4e26-9865-81615998aaa4',
     n8nWebhookUrl: 'https://n8n.guidomuratore.com.ar/webhook/crear-preferencia'
 };
